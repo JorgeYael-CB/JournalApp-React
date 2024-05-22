00514a4a691e0ad37b7f8725198a71fb1f01e2d3
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-22 09:29:20 -0600

    agregando redux y el store

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,39 +2,71 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 
 export const LoginPage = () => {
+  const { status } = useSelector( state => state.auth);
+  const dispatch = useDispatch();
+
+  const {email, password, onInputChange, onResetForm} = useForm({
+    email: 'correo@correo.com',
+    password: 'password',
+  });
+
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+
+    dispatch( checkingAuthentication(email, password) );
+    console.log(status);
+  };
+
+  const onGoogle = () => {
+    dispatch( startGoogleSignIn() );
+    console.log(status);
+  };
+
+
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@google.com' 
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder='correo@google.com'
                 fullWidth
+                name='email'
+                value={email}
+                onChange={ onInputChange }
               />
             </Grid>
 
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder='Contraseña' 
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder='Contraseña'
+                name='password'
+                value={ password }
+                onChange={ onInputChange }
                 fullWidth
               />
             </Grid>
-            
+
             <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
+                <Button type='submit' variant='contained' fullWidth>
                   Login
                 </Button>
               </Grid>
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
+                <Button onClick={onGoogle} variant='contained' fullWidth>
                   <Google />
                   <Typography sx={{ ml: 1 }}>Google</Typography>
                 </Button>
